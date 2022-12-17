6adfd410f57e9109f63e4ee7cb8a163108139281
unknown <vanhai200345@gmail.com>
2022-12-17 09:34:11 +0700

    Btvn ms doi lai

--- Buoi9/Bai2.js
+++ Buoi9/Bai2.js
@@ -2,11 +2,15 @@ const obj = {
     name: "Hai",
     age : 19,
     address: "Nam Dinh"
-}
+};
 
 const run = (Object) => {
-    let arr = [Object];
-
+    let arr = [];
+    var i = 0;
+    for(const key in Object){
+        arr[i] = Object[key];
+        i++; 
+    }
     console.log(arr);
 }
 
